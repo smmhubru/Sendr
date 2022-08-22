a95d78965cdb98e8d81dfb5ab4123d8566877d72
Sergey Simenshteyn <simenshteyn@gmail.com>
2022-08-22 17:42:25 +0700

    init
    Took 6 minutes

---
+++ src/main/java/ru/smmhub/sendr/SendrApplication.java
@@ -0,0 +1,13 @@
+package ru.smmhub.sendr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendrApplication.class, args);
+	}
+
+}
